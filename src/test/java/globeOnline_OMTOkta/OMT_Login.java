38b0cde77946733918eead61d207e37e7c97e70a
ms224836 <109519624+ms224836@users.noreply.github.com>
2023-01-06 10:26:37 +0530

    Update OMT_Login.java

--- src/test/java/globeOnline_OMTOkta/OMT_Login.java
+++ src/test/java/globeOnline_OMTOkta/OMT_Login.java
@@ -2,7 +2,6 @@ package globeOnline_OMTOkta;
 
 import static org.junit.Assert.assertTrue;
 
-
 import java.util.LinkedHashMap;
 
 import org.openqa.selenium.By;
@@ -10,7 +9,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 
 import com.pages.omt.ACQUI_omt_page;
 
@@ -30,7 +28,7 @@ public class OMT_Login extends SetDriver {
 	private String Status = "failed";
 	private static LinkedHashMap<String, String> Map = new LinkedHashMap<>();
 	private String Globeurl = "https://onlinepreprod.globe.com.ph/";
-	private String OMTurl = "https://edo-dev-data-engineering.globe.com.ph/omt-uat/bbsp-admin/";
+	private String OMTurl = "https://edo-data-engineering.globe.com.ph/omt/";
 	private By UserName = By.xpath("//input[@name='username' or @id='okta-signin-username']");
 	private By Password = By.xpath("//input[@name='password']");
 	private By UserName1 = By.xpath("//input[@name='identifier']");
@@ -39,14 +37,23 @@ public class OMT_Login extends SetDriver {
 
 	public void OMT_Role_Login(String FlowType, String role) throws Exception {
 
-		Thread.sleep(8000);
+		Thread.sleep(10000);
+
+		if (OMTAcqui.isElementExist("OKTA Account", "AccountBtn", 20)) {
+			OMTAcqui.clickOnElement("OKTA Account", "AccountBtn", "AccountBtn");
+
+			OMTAcqui.clickOnElement("OKTA SignIn", "OKTA_AccSignIn", "OKTA_AccSignIn");
+		}
+
 		if (OMTAcqui.isElementExist("OKTA SSO button", "OKTA_SSO_Btn", 20)) {
-			Generic.WriteTestData("User should be able to access OMT application", "", "", "User should be able to access OMT application","User is able to access OMT application", "Passed");
+			Generic.WriteTestData("User should be able to access OMT application", "", "",
+					"User should be able to access OMT application", "User is able to access OMT application",
+					"Passed");
 			Thread.sleep(4000);
-		    Control.takeScreenshot();
+			Control.takeScreenshot();
 			OMTAcqui.clickOnElement("OKTA_SSO", "OKTA_SSO_Btn", "OKTA_SSO_Btn");
 		}
-		Thread.sleep(2000);
+		Thread.sleep(3000);
 		// Login as Team Lead
 		String username = util.ReadFromExcel(role, "LoginDetails", Constant.Login_NetworkIDColm);
 		String password = util.ReadFromExcel(role, "LoginDetails", Constant.Login_PasswordColm);
@@ -57,13 +64,14 @@ public class OMT_Login extends SetDriver {
 			Thread.sleep(1000);
 			DriverManager.getDriver().findElement(Password1).sendKeys(password);
 			Thread.sleep(1000);
-			if(OMTAcqui.isElementExist("OMT_Keep me signed in", "KeepMeSignedIn", 10))
-			{
-			DriverManager.getDriver().findElement(OMTAcqui.KeepMeSignedIn).click();
+			if (OMTAcqui.isElementExist("OMT_Keep me signed in", "KeepMeSignedIn", 10)) {
+				DriverManager.getDriver().findElement(OMTAcqui.KeepMeSignedIn).click();
 			}
-			Generic.WriteTestData("User should be able Click the submit Button", "", "", "User should be able to access OMT application","User is able to access OMT application", "Passed");
+			Generic.WriteTestData("User should be able Click the submit Button", "", "",
+					"User should be able to access OMT application", "User is able to access OMT application",
+					"Passed");
 			Thread.sleep(4000);
-		    Control.takeScreenshot();
+			Control.takeScreenshot();
 			OMTAcqui.clickOnElement("Signin Button", "SignIn_Btn", "SignIn_Btn");
 			Thread.sleep(2000);
 
@@ -78,17 +86,20 @@ public class OMT_Login extends SetDriver {
 		}
 
 		if (OMTAcqui.isElementExist("Get Okta Notification", "OKTA_Notif", 10)) {
-			Generic.WriteTestData("User should be able to access OMT application", "", "", "User should be able to access OMT application","User is able to access OMT application", "Passed");
+			Generic.WriteTestData("User should be able to access OMT application", "", "",
+					"User should be able to access OMT application", "User is able to access OMT application",
+					"Passed");
 			Thread.sleep(4000);
-		    Control.takeScreenshot();
+			Control.takeScreenshot();
 			OMTAcqui.clickOnElement("Get Okta push Notification", "OKTA_Notif", "OKTA_Notif");
-			
+
 		}
-		Thread.sleep(50000);
+		Thread.sleep(5000);
 		assertTrue(DriverManager.getDriver().getTitle().contains("Orders Portal"));
-		Generic.WriteTestData("User should be able to access Header Order Portal", "", "", "User should be able to access OMT application","User is able to access OMT application", "Passed");
+		Generic.WriteTestData("User should be able to access Header Order Portal", "", "",
+				"User should be able to access OMT application", "User is able to access OMT application", "Passed");
 		Thread.sleep(4000);
-	    Control.takeScreenshot();
+		Control.takeScreenshot();
 		System.out.println("Validated title : Orders Portal");
 
 	}
